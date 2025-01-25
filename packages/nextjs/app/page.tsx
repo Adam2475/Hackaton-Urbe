2ebb456d2dc8fc5109b9a3fc242d60afe0990158
Adam2475 <adamnatanpassaretti@gmail.com>
2025-01-25 18:48:35 +0100

    added account balance view

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -10,9 +10,9 @@ export default function Login() {
   const { address, isConnected } = useAccount();
   const { connect } = useConnect();
   const { disconnect } = useDisconnect();
-  const result = useBalance({
-    address: '0xf4A4EeD4A13eE7ED44d01ef3896Ae79677d060f6',
-  })
+  const { data, isError, isLoading } = useBalance({
+    address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+  });
 
   return (
     <Container maxWidth="sm" style={{ textAlign: "center", paddingTop: "50px" }}>
@@ -20,6 +20,11 @@ export default function Login() {
         Safepayaments
       </Typography>
 
+     
+      <div>
+      Balance: {data?.formatted} {data?.symbol}
+    </div>
+  
       {!isConnected ? (
         <Button
           variant="contained"
@@ -33,7 +38,9 @@ export default function Login() {
           <Typography variant="h6">Wallet: {address}</Typography>
           <Button variant="contained" color="secondary" onClick={() => disconnect()} sx={{ mt: 2 }}>
             Disconnetti
+            
           </Button>
+         
         </Box>
       )}
 
